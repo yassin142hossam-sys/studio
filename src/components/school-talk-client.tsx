f485677c9364aca887de5507067f8d98787cfcb4
yassin <yassin142.hossam@gmail.com>
2025-09-17 20:55:14 +0000

    in the message that will be sent i need the first sentence to be مع حضرت

--- src/components/school-talk-client.tsx
+++ src/components/school-talk-client.tsx
@@ -279,7 +279,7 @@ export function SchoolTalkClient() {
     }
     const teacherPhoneNumber = user?.email?.split('@')[0];
     const date = new Date().toLocaleDateString();
-    const message = `[${date}] Message from teacher Hanaa Abdel-Majid regarding student: ${foundStudent.name}\n\n- ${messageParts.join('\n\n- ')}`;
+    const message = `[${date}] مع حضرتك اسيستنت Mrs. Hanaa Abdel-Majid بنبلغ حضرتك بأداء الطالب/ة: ${foundStudent.name}\n\n- ${messageParts.join('\n\n- ')}`;
 
     const url = `whatsapp://send?phone=${foundStudent.parentWhatsApp}&text=${encodeURIComponent(message)}`;
     window.open(url, "_blank");
